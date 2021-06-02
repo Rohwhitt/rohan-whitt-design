35a5e8b0c350fe7ed4adcf886804afe4c242ee85
Rohwhitt <70130968+Rohwhitt@users.noreply.github.com>
2021-06-02 12:07:11 +0800

    Project Added
    - New project was added as project 1
    - Changed explore animation

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ $(document).ready(function () {
   var handAnimation = new TimelineMax({ onUpdate: updateHand }); //Variable for all hand animations on scroll
   var titleAnimation = new TimelineMax(); //Variable for title animation on scroll
   var mouseAnimation = new TimelineMax(); // Variable for mouse animation on scroll
+  var logoAnimation = new TimelineMax(); // Variable for logo text on scroll
   var aboutAnimation = new TimelineMax(); //Variable for about section animation
   var visualAnimation = new TimelineMax(); //Variable for about visuals animation
+  var workAnimation = new TimelineMax();
+  var projectBgAnimation = new TimelineMax();
+  var projectAnimation = new TimelineMax();
+  var projectNumber = new TimelineMax();
   var parallaxScroll = new TimelineMax({ onUpdate: updateParallax }); //parallax scrolling for project 1
   var parallaxScrollTwo = new TimelineMax({ onUpdate: updateParallaxTwo }); //parallax scrolling for project 2
   const controller = new ScrollMagic.Controller();
@@ -38,12 +43,34 @@ $(document).ready(function () {
   //Title and Scroll animations
   titleAnimation.to(".handpin h1", 0.2, { opacity: 0 });
   mouseAnimation.to(".mousescroll", 0.2, { display: "none" });
+  logoAnimation.to(".nav-logo", 0.4, { opacity: 0 });
   //About animations
-  aboutAnimation.to("#about h1, .about-text", 0.5, { y: -30, opacity: 1 });
-  visualAnimation.to(".about-visuals", 0.7, { opacity: 1 }, "=0.5");
+  aboutAnimation.to("#about h1, .about-text", 1.2, { y: -30, opacity: 1 });
+  visualAnimation.to(".about-visuals", 1, { opacity: 1 }, "=0.5");
+  //Work Animations
+  workAnimation.to("#work h1", 1.2, { opacity: 1 });
+  projectBgAnimation.to(".image-container", 1, { opacity: 1 }, "=0.5");
+  projectNumber.to(".content-align h4", 1, { opacity: 0.2 }, "=0.7");
+  projectAnimation.to(
+    ".content-align h2, .content-align h3, .button-main",
+    1,
+    { opacity: 1 },
+    "=0.7"
+  );
   //Parallax Work animation
   parallaxScroll.to("#projectbg1", 1, { y: "-20%", ease: Power0.easeNone });
   parallaxScrollTwo.to("#projectbg2", 1, { y: "-20%", ease: Power0.easeNone });
+
+  // scrollMagic scene function
+  const newScene = function (scrollPoint, triggerNumber, runAmount, animation) {
+    const scene10 = new ScrollMagic.Scene({
+      triggerElement: scrollPoint,
+      triggerHook: triggerNumber,
+      reverse: runAmount,
+    })
+      .setTween(animation)
+      .addTo(controller);
+  };
 
   const scene = new ScrollMagic.Scene({
     triggerElement: ".scrollpoint1",
@@ -54,37 +81,41 @@ $(document).ready(function () {
     .setPin(".handpin")
     .setTween(handAnimation)
     .addTo(controller);
-
+  /*
   const scene2 = new ScrollMagic.Scene({
     triggerElement: ".scrollpoint1",
     triggerHook: "1",
   })
     .setTween(titleAnimation)
     .addTo(controller);
-
-  const sceneThree = new ScrollMagic.Scene({
-    triggerElement: ".scrollpoint1",
-    triggerHook: "1",
-  })
-    .setTween(mouseAnimation)
-    .addTo(controller);
-
+*/
+  newScene(".scrollpoint1", "1", true, titleAnimation);
+  newScene(".scrollpoint1", "1", true, mouseAnimation);
+  newScene(".scrollpoint2", "1", true, logoAnimation);
+  newScene("#about", 0.6, false, aboutAnimation);
+  newScene("#about", 0.6, false, visualAnimation);
+  newScene(".project1", 0.6, false, workAnimation);
+  newScene(".project1", 0.5, false, projectBgAnimation);
+  newScene(".project1", 0.5, false, projectNumber);
+  newScene(".project1", 0.5, false, projectAnimation);
+  /*
   const scene3 = new ScrollMagic.Scene({
     triggerElement: "#about",
-    triggerHook: ".4",
+    triggerHook: 0.6,
     reverse: false,
   })
+    .addIndicators()
     .setTween(aboutAnimation)
     .addTo(controller);
 
   const scene4 = new ScrollMagic.Scene({
     triggerElement: "#about",
-    triggerHook: ".4",
+    triggerHook: 0.6,
     reverse: false,
   })
     .setTween(visualAnimation)
     .addTo(controller);
-
+*/
   const scene5 = new ScrollMagic.Scene({
     triggerElement: "#project1",
     triggerHook: ".7",
